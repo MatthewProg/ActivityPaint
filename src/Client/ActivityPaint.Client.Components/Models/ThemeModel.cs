dea3a0381956d77dbe26b7e81601c1cb521a1b9e
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-05-30 18:48:35 +0200

    Add theme model, editor theme overwrite

---
+++ src/Client/ActivityPaint.Client.Components/Models/ThemeModel.cs
@@ -0,0 +1,23 @@
+﻿using ActivityPaint.Client.Components.Enums;
+using ActivityPaint.Client.Components.Helpers;
+using MudBlazor;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ActivityPaint.Client.Components.Models;
+
+public class ThemeModel
+{
+    public required MudTheme MudTheme { get; set; }
+    public required ThemeEnum Theme { get; set; }
+    public ThemeEnum? ResolvedTheme { get; set; }
+
+    public bool IsDarkMode => (ResolvedTheme ?? Theme) == ThemeEnum.Dark;
+
+    [SetsRequiredMembers]
+    public ThemeModel()
+    {
+        MudTheme = ThemeHelper.GetDefaultTheme();
+        Theme = ThemeEnum.System;
+        ResolvedTheme = null;
+    }
+}
