be0920a4ccd0f730c9b5e19eadf53d510590a598
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-06-01 18:30:54 +0200

    Refactor editor

---
+++ src/Client/ActivityPaint.Client.Components/Models/EditorSettingsModel.cs
@@ -0,0 +1,82 @@
+﻿using ActivityPaint.Client.Components.Enums;
+using ActivityPaint.Core.Enums;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ActivityPaint.Client.Components.Models;
+
+public class EditorSettingsModel : INotifyPropertyChanged
+{
+    private bool _isDarkMode;
+    private DateTime? _startDate;
+    private int _brushSize;
+    private EditorToolEnum _selectedTool;
+    private IntensityEnum _selectedIntensity;
+
+    public bool IsDarkMode
+    {
+        get => _isDarkMode;
+        set
+        {
+            if (_isDarkMode == value) return;
+
+            _isDarkMode = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public DateTime? StartDate
+    {
+        get => _startDate;
+        set
+        {
+            if (_startDate == value) return;
+
+            _startDate = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int BrushSize
+    {
+        get => _brushSize;
+        set
+        {
+            if (_brushSize == value) return;
+
+            _brushSize = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public EditorToolEnum SelectedTool
+    {
+        get => _selectedTool;
+        set
+        {
+            if (_selectedTool == value) return;
+
+            _selectedTool = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public IntensityEnum SelectedIntensity
+    {
+        get => _selectedIntensity;
+        set
+        {
+            if (_selectedIntensity == value) return;
+
+            _selectedIntensity = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
--- src/Client/ActivityPaint.Client.Components/UI/Editor/EditorCanvasComponent.razor.js
+++ src/Client/ActivityPaint.Client.Components/UI/Editor/EditorCanvasComponent.razor.js
@@ -1,17 +1,7 @@
-﻿function resizeCanvas() {
-    console.log('resize called');
-    const canvas = document.getElementById('editor-canvas');
-    const cell = canvas.getElementsByTagName('td')[0];
-    const cellWidth = cell.clientWidth + 2;
+﻿export function init() {
 
-    canvas.style.setProperty('--canvas-item-width', `${cellWidth}px`);
-}
-
-export function init() {
-    addEventListener('resize', resizeCanvas);
-    resizeCanvas(undefined);
 }
 
 export function destroy() {
-    removeEventListener('resize', resizeCanvas);
+
 }
