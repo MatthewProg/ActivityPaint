36e2831c3646ae37e22bdab11f6773935a413779
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-08-04 20:35:07 +2000

    Compress and encode canvas data

--- src/ActivityPaint.Core/Helpers/CanvasDataHelper.cs
+++ src/ActivityPaint.Core/Helpers/CanvasDataHelper.cs
@@ -1,4 +1,5 @@
 ﻿using ActivityPaint.Core.Enums;
+using System.IO.Compression;
 
 namespace ActivityPaint.Core.Helpers;
 
@@ -8,19 +9,48 @@ public static class CanvasDataHelper
     {
         ArgumentNullException.ThrowIfNull(canvasData);
 
-        var charArray = canvasData.Select(x => (char)(x + '0'))
-                                  .ToArray();
+        var bytes = canvasData.Select(x => (byte)x)
+                              .ToArray();
 
-        return new string(charArray);
+        if (bytes.Length == 0)
+        {
+            return string.Empty;
+        }
+
+        using var outputStream = new MemoryStream();
+        using var compressionStream = new ZLibStream(outputStream, CompressionLevel.Fastest);
+
+        compressionStream.Write(bytes);
+        compressionStream.Flush();
+
+        var compressedBytes = outputStream.ToArray();
+        var output = Convert.ToBase64String(compressedBytes);
+
+        return output;
     }
 
     public static List<IntensityEnum> ConvertToList(string canvasDataString)
     {
         ArgumentNullException.ThrowIfNull(canvasDataString);
 
-        var array = canvasDataString.Select(x => (IntensityEnum)(x - '0'))
-                                    .ToList();
+        if (canvasDataString == string.Empty)
+        {
+            return [];
+        }
 
-        return array;
+        var bytes = Convert.FromBase64String(canvasDataString);
+
+        using var inputStream = new MemoryStream(bytes);
+        using var outputStream = new MemoryStream();
+        using var compressionStream = new ZLibStream(inputStream, CompressionMode.Decompress);
+
+        compressionStream.CopyTo(outputStream);
+        compressionStream.Flush();
+
+        var decompressedBytes = outputStream.ToArray();
+        var output = decompressedBytes.Select(x => (IntensityEnum)x)
+                                      .ToList();
+
+        return output;
     }
 }
