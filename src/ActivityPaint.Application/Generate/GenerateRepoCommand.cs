1cd95419d3d3ea4c4acab71a389384ad7b7e7324
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-08-21 01:23:13 +0200

    Add commits generation logic

---
+++ src/ActivityPaint.Application/Generate/GenerateRepoCommand.cs
@@ -0,0 +1,48 @@
+﻿using ActivityPaint.Application.Abstractions.Repository;
+using ActivityPaint.Application.Abstractions.Repository.Models;
+using ActivityPaint.Application.BusinessLogic.Generate.Services;
+using ActivityPaint.Application.BusinessLogic.Shared.Mediator;
+using ActivityPaint.Application.DTOs.Extensions;
+using ActivityPaint.Application.DTOs.Models;
+using ActivityPaint.Core.Shared.Progress;
+using ActivityPaint.Core.Shared.Result;
+using FluentValidation;
+
+namespace ActivityPaint.Application.BusinessLogic.Generate;
+
+public record GenerateRepoCommand(
+    PresetModel Preset,
+    AuthorModel Author,
+    string Path,
+    Progress? ProgressCallback = null
+) : IResultRequest;
+
+internal class GenerateRepoCommandValidator : AbstractValidator<GenerateRepoCommand>
+{
+    public GenerateRepoCommandValidator(IEnumerable<IValidator<PresetModel>> presetValidators)
+    {
+        RuleFor(x => x.Preset)
+            .NotNull()
+            .SetDefaultValidator(presetValidators);
+    }
+}
+
+internal class GenerateRepoCommandHandler : IResultRequestHandler<GenerateRepoCommand>
+{
+    private const string DEFAULT_MESSAGE_FORMAT = "ActivityPaint - '{name}' - (Commit {current_total}/{total_count})";
+    private readonly IRepositoryService _repositoryService;
+    private readonly ICommitsService _commitsService;
+
+    public GenerateRepoCommandHandler(IRepositoryService repositoryService, ICommitsService commitsService)
+    {
+        _repositoryService = repositoryService;
+        _commitsService = commitsService;
+    }
+
+    public ValueTask<Result> Handle(GenerateRepoCommand request, CancellationToken cancellationToken)
+    {
+        var commits = _commitsService.GenerateCommits(request.Preset, DEFAULT_MESSAGE_FORMAT);
+
+        return ValueTask.FromResult(_repositoryService.InitOrPopulateRepository(request.Path, request.Author, commits, request.ProgressCallback));
+    }
+}
---
+++ src/ActivityPaint.Application/Generate/Services/CommitsService.cs
@@ -0,0 +1,69 @@
+﻿using ActivityPaint.Application.Abstractions.Repository.Models;
+using ActivityPaint.Application.DTOs.Models;
+using System.Text.RegularExpressions;
+
+namespace ActivityPaint.Application.BusinessLogic.Generate.Services;
+
+internal interface ICommitsService
+{
+    List<CommitModel> GenerateCommits(PresetModel model, string messageFormat);
+}
+
+internal partial class CommitsService : ICommitsService
+{
+    public List<CommitModel> GenerateCommits(PresetModel model, string messageFormat)
+    {
+        var metadata = GetMetadata(model);
+        var output = new List<CommitModel>(metadata.TotalCommits);
+
+        foreach (var commitsCount in model.CanvasData.Select(x => (int)x))
+        {
+            metadata.CurrentDay++;
+
+            if (commitsCount == 0) continue;
+
+            for (metadata.CurrentDayCommit = 1; metadata.CurrentDayCommit <= commitsCount; metadata.CurrentDayCommit++, metadata.CurrentTotalCommit++)
+            {
+                var message = TokenRegex().Replace(messageFormat, (match) => GetTokenValue(metadata, match.Value));
+                var date = metadata.StartDay.AddDays(metadata.CurrentDay);
+
+                output.Add(new(message, date));
+            }
+        }
+
+        return output;
+    }
+
+    private static Metadata GetMetadata(PresetModel model) => new(
+        Name: model.Name,
+        StartDay: new DateTimeOffset(model.StartDate.Date, TimeSpan.FromHours(12)),
+        CurrentDay: 0,
+        CurrentDayCommit: 0,
+        CurrentTotalCommit: 0,
+        TotalCommits: model.CanvasData.Aggregate(0, (total, current) => total + (int)current)
+    );
+
+    private static string GetTokenValue(Metadata metadata, string token) => token switch
+    {
+        "name" => metadata.Name,
+        "start_date" => metadata.StartDay.ToString("yyyy-MM-dd"),
+        "current_date" => metadata.StartDay.AddDays(metadata.CurrentDay).ToString("yyyy-MM-dd"),
+        "current_day" => metadata.CurrentDay.ToString(),
+        "current_day_commit" => metadata.CurrentDayCommit.ToString(),
+        "current_total" => metadata.CurrentTotalCommit.ToString(),
+        "total_count" => metadata.TotalCommits.ToString(),
+        _ => token
+    };
+
+    [GeneratedRegex(@"((?<={)\w*(?=}))*")]
+    private static partial Regex TokenRegex();
+
+    private record struct Metadata(
+        string Name,
+        DateTimeOffset StartDay,
+        int CurrentDay,
+        int CurrentDayCommit,
+        int CurrentTotalCommit,
+        int TotalCommits
+    );
+}
