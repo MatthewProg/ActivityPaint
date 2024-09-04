396832c1b72357219ecb0e0ccc5f6f7f93d1fde2
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-09-04 23:30:13 +0200

    Generate git commands

---
+++ src/ActivityPaint.Application/Files/SaveTextToFileCommand.cs
@@ -0,0 +1,57 @@
+﻿using ActivityPaint.Application.Abstractions.FileSystem;
+using ActivityPaint.Application.Abstractions.Interactions;
+using ActivityPaint.Application.BusinessLogic.Shared.Mediator;
+using ActivityPaint.Application.DTOs.Shared.Extensions;
+using ActivityPaint.Core.Shared.Result;
+using FluentValidation;
+using System.Text;
+
+namespace ActivityPaint.Application.BusinessLogic.Files;
+
+public sealed record SaveTextToFileCommand(
+    string Text,
+    string? Path = null,
+    bool Overwrite = false
+) : IResultRequest;
+
+internal class SaveTextToFileCommandValidator : AbstractValidator<SaveTextToFileCommand>
+{
+    public SaveTextToFileCommandValidator()
+    {
+        RuleFor(x => x.Text)
+            .NotNull();
+
+        RuleFor(x => x.Path)
+            .Path();
+    }
+}
+
+internal class SaveTextToFileCommandHandler : IResultRequestHandler<SaveTextToFileCommand>
+{
+    private readonly IFileSystemInteraction _fileSystemInteraction;
+    private readonly IFileSaveService _fileSaveService;
+
+    public SaveTextToFileCommandHandler(IFileSystemInteraction fileSystemInteraction, IFileSaveService fileSaveService)
+    {
+        _fileSystemInteraction = fileSystemInteraction;
+        _fileSaveService = fileSaveService;
+    }
+
+    public async ValueTask<Result> Handle(SaveTextToFileCommand command, CancellationToken cancellationToken)
+    {
+        using var data = new MemoryStream();
+        using var writer = new StreamWriter(data, Encoding.UTF8);
+
+        await writer.WriteAsync(command.Text);
+        await writer.FlushAsync(cancellationToken);
+
+        data.Seek(0, SeekOrigin.Begin);
+
+        if (string.IsNullOrWhiteSpace(command.Path))
+        {
+            return await _fileSystemInteraction.PromptFileSaveAsync("save.txt", data, cancellationToken);
+        }
+
+        return await _fileSaveService.SaveFileAsync(command.Path, data, command.Overwrite, cancellationToken);
+    }
+}
---
+++ src/ActivityPaint.Application/Generate/GenerateGitCmdCommand.cs
@@ -0,0 +1,69 @@
+﻿using ActivityPaint.Application.BusinessLogic.Generate.Services;
+using ActivityPaint.Application.BusinessLogic.Shared.Mediator;
+using ActivityPaint.Application.DTOs.Preset;
+using ActivityPaint.Application.DTOs.Shared.Extensions;
+using ActivityPaint.Core.Shared.Progress;
+using ActivityPaint.Core.Shared.Result;
+using FluentValidation;
+using System.Text;
+
+namespace ActivityPaint.Application.BusinessLogic.Generate;
+
+public sealed record GenerateGitCmdCommand(
+    PresetModel Preset,
+    string? MessageFormat = null,
+    Progress? ProgressCallback = null
+) : IResultRequest<string>;
+
+internal class GenerateGitCmdCommandValidator : AbstractValidator<GenerateGitCmdCommand>
+{
+    public GenerateGitCmdCommandValidator(IEnumerable<IValidator<PresetModel>> presetValidators)
+    {
+        RuleFor(x => x.Preset)
+            .NotNull()
+            .SetDefaultValidator(presetValidators);
+    }
+}
+
+internal class GenerateGitCmdCommandHandler : IResultRequestHandler<GenerateGitCmdCommand, string>
+{
+    private readonly ICommitsService _commitsService;
+
+    public GenerateGitCmdCommandHandler(ICommitsService commitsService)
+    {
+        _commitsService = commitsService;
+    }
+
+    public async ValueTask<Result<string>> Handle(GenerateGitCmdCommand request, CancellationToken cancellationToken)
+    {
+        var commits = _commitsService.GenerateCommits(request.Preset, request.MessageFormat);
+
+        if (commits.Count == 0)
+        {
+            return string.Empty;
+        }
+
+        var firstCommit = commits[0];
+        var sizeApprox = GetCommand(firstCommit.DateTime, firstCommit.Message).Length * commits.Count;
+        var builder = new StringBuilder(sizeApprox);
+
+        for (int i = 0; i < commits.Count; i++)
+        {
+            var commit = commits[i];
+            var message = GetCommand(commit.DateTime, commit.Message);
+
+            if (i == commits.Count - 1)
+            {
+                message = message[..^1];
+            }
+
+            builder.Append(message);
+        }
+
+        return builder.ToString();
+    }
+
+    private static string GetCommand(DateTimeOffset date, string message)
+        => $"git commit --allow-empty --no-verify --date={date:O} -m \"{message}\";\n";
+}
+
