f648919a410564183592863411bbfcbcfa027191
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-09-24 16:37:09 +0200

    Add CliCmd command validator tests

---
+++ test/ActivityPaint.Application.BusinessLogic.Tests/ValidatorMockFactory.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace ActivityPaint.Application.BusinessLogic.Tests;
+
+public static class ValidatorMockFactory
+{
+    public static Mock<IValidator<T>> CreateValid<T>(Times? callCount = null)
+    {
+        var mock = new Mock<IValidator<T>>();
+
+        var setup = mock.Setup(x => x.Validate(It.IsAny<IValidationContext>()))
+                        .Returns(new ValidationResult());
+
+        if (callCount is not null)
+        {
+            setup.Verifiable(callCount.Value);
+        }
+
+        return mock;
+    }
+}
