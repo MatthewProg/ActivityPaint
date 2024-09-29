d43b4a767338a5c37455f502423b646f6496b131
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-09-29 14:33:16 +0200

    Add Repository tests

---
+++ test/ActivityPaint.Application.BusinessLogic.Tests/Repository/GetRepositoryConfigCommandTests.cs
@@ -0,0 +1,59 @@
+﻿using ActivityPaint.Application.Abstractions.Database.Repositories;
+using ActivityPaint.Application.BusinessLogic.Repository;
+using ActivityPaint.Application.DTOs.Repository;
+using ActivityPaint.Core.Entities;
+
+namespace ActivityPaint.Application.BusinessLogic.Tests.Repository;
+
+public class GetRepositoryConfigCommandTests
+{
+    [Fact]
+    public async Task Handle_WhenNoConfigs_ShouldReturnModelWithNulls()
+    {
+        // Arrange
+        var expected = new RepositoryConfigModel(null, null, null);
+        var cancellationToken = new CancellationToken();
+        var mock = new Mock<IRepositoryConfigRepository>();
+        mock.Setup(x => x.GetFirstAsync(It.Is<CancellationToken>(x => x.Equals(cancellationToken))))
+            .ReturnsAsync((RepositoryConfig)null!)
+            .Verifiable(Times.Once);
+
+        var command = new GetRepositoryConfigCommand();
+        var service = new GetRepositoryConfigCommandHandler(mock.Object);
+
+        // Act
+        var result = await service.Handle(command, cancellationToken);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(expected);
+    }
+
+    [Fact]
+    public async Task Handle_WhenGotConfigs_ShouldReturnFirst()
+    {
+        // Arrange
+        var expected = new RepositoryConfigModel("{name} commit", "test@example.com", "Unit Test");
+        var cancellationToken = new CancellationToken();
+        var mock = new Mock<IRepositoryConfigRepository>();
+        mock.Setup(x => x.GetFirstAsync(It.Is<CancellationToken>(x => x.Equals(cancellationToken))))
+            .ReturnsAsync(new RepositoryConfig()
+            {
+                Id = 1,
+                AuthorEmail = "test@example.com",
+                AuthorFullName = "Unit Test",
+                MessageFormat = "{name} commit"
+            })
+            .Verifiable(Times.Once);
+
+        var command = new GetRepositoryConfigCommand();
+        var service = new GetRepositoryConfigCommandHandler(mock.Object);
+
+        // Act
+        var result = await service.Handle(command, cancellationToken);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(expected);
+    }
+}
---
+++ test/ActivityPaint.Application.BusinessLogic.Tests/Repository/UpdateRepositoryConfigCommandValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using ActivityPaint.Application.BusinessLogic.Repository;
+
+namespace ActivityPaint.Application.BusinessLogic.Tests.Repository;
+
+public class UpdateRepositoryConfigCommandValidatorTests
+{
+    [Fact]
+    public void WhenAllCorrect_ShouldBeValid()
+    {
+        // Arrange
+        var validator = new UpdateRepositoryConfigCommandValidator();
+        var model = GetValidModel();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void WhenModelIsNull_ShouldBeInvalid()
+    {
+        // Arrange
+        var validator = new UpdateRepositoryConfigCommandValidator();
+        var model = GetValidModel() with { Model = null! };
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Model);
+    }
+
+    private static UpdateRepositoryConfigCommand GetValidModel() => new(
+        Model: new("{name} commit", "test@example.com", "Unit Test")
+    );
+}
