0447ae8b472e8a3f1c09012533ddea2f2d9b089b
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-10-06 10:38:54 +0200

    Abstract docs page template

---
+++ src/Client/ActivityPaint.Client.Components/Documentation/Shared/IDocumentationPage.cs
@@ -0,0 +1,10 @@
+﻿namespace ActivityPaint.Client.Components.Documentation.Shared;
+
+public interface IDocumentationPage
+{
+    public string Title { get; }
+    public string Command { get; }
+    public IEnumerable<string> Aliases { get; }
+    public IEnumerable<DocumentationOptionsTableModel> Options { get; }
+    public IEnumerable<string> Examples { get; }
+}
