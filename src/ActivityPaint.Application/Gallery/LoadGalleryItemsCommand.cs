6610cc5abfba4cdecd7603af2e7c20fa00eb0a3b
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-10-22 22:04:43 +0200

    Add repository paging method, create basic layout

---
+++ src/ActivityPaint.Application/Gallery/LoadGalleryItemsCommand.cs
@@ -0,0 +1,25 @@
+﻿using ActivityPaint.Application.Abstractions.Database.Repositories;
+using ActivityPaint.Application.BusinessLogic.Shared.Mediator;
+using ActivityPaint.Application.DTOs.Preset;
+using ActivityPaint.Core.Shared.Result;
+
+namespace ActivityPaint.Application.BusinessLogic.Gallery;
+
+public record LoadGalleryItemsCommand(
+    int Page,
+    int PageSize = 18
+) : IResultRequest<IEnumerable<PresetModel>>;
+
+internal class LoadGalleryItemsCommandHandler(IPresetRepository presetRepository) : IResultRequestHandler<LoadGalleryItemsCommand, IEnumerable<PresetModel>>
+{
+    private readonly IPresetRepository _presetRepository = presetRepository;
+
+    public async ValueTask<Result<IEnumerable<PresetModel>>> Handle(LoadGalleryItemsCommand request, CancellationToken cancellationToken)
+    {
+        var page = await _presetRepository.GetPageAsync(request.Page, request.PageSize, cancellationToken);
+
+        var result = page.Select(x => x.ToPresetModel());
+
+        return Result<IEnumerable<PresetModel>>.Success(result);
+    }
+}
--- src/Integration/ActivityPaint.Integration.Database/Repositories/PresetRepository.cs
+++ src/Integration/ActivityPaint.Integration.Database/Repositories/PresetRepository.cs
@@ -1,8 +1,22 @@
 ﻿using ActivityPaint.Application.Abstractions.Database.Repositories;
 using ActivityPaint.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ActivityPaint.Integration.Database.Repositories;
 
 internal class PresetRepository(ActivityPaintContext context) : GenericRepository<Preset>(context), IPresetRepository
 {
+    public async ValueTask<List<Preset>> GetPageAsync(int page, int pageSize, CancellationToken cancellationToken = default)
+    {
+        var normalizedPage = page - 1;
+
+        var result = await _context.Presets
+                                   .AsNoTracking()
+                                   .OrderBy(p => p.Id)
+                                   .Skip(normalizedPage * pageSize)
+                                   .Take(pageSize)
+                                   .ToListAsync(cancellationToken);
+
+        return result;
+    }
 }
