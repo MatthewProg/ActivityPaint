bdfe0c8d7e7d0326e8c6b8b56be182e75ef396ef
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-11-01 20:22:43 +0100

    Create save dialog

---
+++ src/Client/ActivityPaint.Client.Components/Gallery/GalleryModelMap.cs
@@ -0,0 +1,15 @@
+﻿using ActivityPaint.Application.DTOs.Gallery;
+using ActivityPaint.Client.Components.Editor;
+
+namespace ActivityPaint.Client.Components.Gallery;
+
+public static class GalleryModelMap
+{
+    public static EditorModel ToEditorModel(this GalleryModel galleryModel) => new()
+    {
+        IsDarkModeDefault = galleryModel.IsDarkModeDefault,
+        CanvasData = galleryModel.CanvasData,
+        StartDate = galleryModel.StartDate,
+        Name = galleryModel.Name
+    };
+}
