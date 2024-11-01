369fc4ab2fc93a8da0b0b3eace489da351484174
Mateusz Ciaglo <mat.ciaglo@gmail.com>
2024-11-01 17:39:07 +0100

    Add item deletion, fix re-renders and incorrect alignment

---
+++ src/ActivityPaint.Application/Gallery/DeleteGalleryItemCommand.cs
@@ -0,0 +1,21 @@
+﻿using ActivityPaint.Application.Abstractions.Database.Repositories;
+using ActivityPaint.Application.BusinessLogic.Shared.Mediator;
+using ActivityPaint.Core.Shared.Result;
+
+namespace ActivityPaint.Application.BusinessLogic.Gallery;
+
+public record DeleteGalleryItemCommand(
+    int Id
+) : IResultRequest;
+
+internal class DeleteGalleryItemCommandHandler(IPresetRepository presetRepository) : IResultRequestHandler<DeleteGalleryItemCommand>
+{
+    private readonly IPresetRepository _presetRepository = presetRepository;
+
+    public async ValueTask<Result> Handle(DeleteGalleryItemCommand request, CancellationToken cancellationToken)
+    {
+        await _presetRepository.DeleteAsync(request.Id, cancellationToken);
+
+        return Result.Success();
+    }
+}
